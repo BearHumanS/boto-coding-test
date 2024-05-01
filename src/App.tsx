ffef95340a31f72c2d0699ef7e73a9efb6cc9f38
BearHumanS <bye0651@naver.com>
2024-05-01 17:54:58 +0900

    카드 플립 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [flipped, setFlipped] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onFlip = () => {
+    if (!isLoading) {
+      setIsLoading(true);
+      setFlipped((prev) => !prev);
+
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 500);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      className={`card-container ${isLoading ? 'disabled' : ''}`}
+      onClick={onFlip}
+    >
+      <div className={`card ${flipped ? 'flipped' : ''}`}>
+        <div className="front">
+          <img src="/front.webp" alt="앞면" width="200" height="290" />
+        </div>
+        <div className="back">
+          <img src="/back.webp" alt="뒷면" width="200" height="290" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
